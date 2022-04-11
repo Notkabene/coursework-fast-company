ea30a09f028be88ff6a484a6d008d646903d9bfe
Notkabene <len1618@yandex.ru>
2022-04-11 22:07:02 +0300

    Выполнены действия из урока и исправлены ошибки

---
+++ src/components/groupList.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const GroupList = ({ items, valueProperty, contentPropety, onItemSelect, selectedItem }) => {
+  return (
+  <ul className="list-group">
+    {Object.keys(items).map(item => (
+      <li
+        key={items[item][valueProperty]}
+        className={'list-group-item' + (items[item] === selectedItem ? ' active' : '')}
+        onClick={() => onItemSelect(items[item])}
+        role='button'
+      >
+        {items[item][contentPropety]}
+      </li>
+    ))}
+  </ul>
+  )
+}
+
+GroupList.defaultProps = {
+  valueProperty: '_id',
+  contentPropety: 'name'
+}
+
+GroupList.propTypes = {
+  items: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  valueProperty: PropTypes.string.isRequired,
+  contentPropety: PropTypes.string.isRequired,
+  onItemSelect: PropTypes.func,
+  selectedItem: PropTypes.object
+}
+
+export default GroupList
