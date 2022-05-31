fef798c8b91993827ec18b32eef1fd7fd34ea69a
Notkabene <len1618@yandex.ru>
2022-06-01 00:19:32 +0300

    Просмотрены видео

--- src/app/App.js
+++ src/app/App.js
@@ -5,18 +5,23 @@ import Users from "./layouts/users";
 import Login from "./layouts/login";
 import Main from "./layouts/main";
 import NavBar from "./components/ui/navBar";
+import { ToastContainer } from "react-toastify";
+import { ProfessionProvider } from "./hooks/useProfession";
 
 function App() {
     return (
-        <div>
-            <NavBar />
-            <Switch>
-                <Route path="/users/:userId?/:edit?" component={Users} />
-                <Route path="/login/:type?" component={Login} />
-                <Route path="/" exact component={Main} />
-                <Redirect to="/" />
-            </Switch>
-        </div>
+      <div>
+      <NavBar />
+      <ProfessionProvider>
+          <Switch>
+              <Route path="/users/:userId?/:edit?" component={Users} />
+              <Route path="/login/:type?" component={Login} />
+              <Route path="/" exact component={Main} />
+              <Redirect to="/" />
+          </Switch>
+      </ProfessionProvider>
+      <ToastContainer />
+  </div>
     );
 }
 
