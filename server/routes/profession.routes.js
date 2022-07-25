44ad2164e6282f0951bd0efd119a1bd82ecda864
Notkabene <len1618@yandex.ru>
2022-07-25 20:40:14 +0300

    backend

---
+++ server/routes/profession.routes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const Profession = require('../models/Profession')
+const router = express.Router({mergeParams: true})
+
+router.get('/', async (req, res) => {
+  try {
+    const list = await Profession.find()
+    console.log("list", list)
+    res.status(200).send(list)
+  } catch (e) {
+    res.status(500).json({message: "На сервере произошла ошибка. попробуйте позже"})
+  }
+})
+
+module.exports = router
